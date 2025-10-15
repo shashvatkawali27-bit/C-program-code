ffef6abd2e79f8172c8aa90f2c5ac58dfdbb735c
shashvatkawali27-bit <shashvatkawali27@eng.rizvi.edu.in>
2025-10-15 09:51:39 +0530

    Create Experiment2Task1.c

---
+++ Experiment2Task1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+//name:-shashvat nilesh kawali
+//class:-comp
+//division:-c
+//UIN:-2501P023
+int number;
+printf("what's your number:-");
+scanf("%d",&number);
+if(number%2==0)
+{ printf("the number is even");
+}
+else {
+printf("the number is odd"):
+  }
+return 0;
+}
